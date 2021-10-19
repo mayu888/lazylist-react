db3d6be3acb691a91fc97a32d1d8cea4dbaa5fff
mayu <mayu827@163.com>
2021-10-19 14:27:39 +0800

    add type

---
+++ lib/typings.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="react" />
+export interface CommonProps {
+    warpTag?: string;
+    className?: string;
+    tag?: string;
+    renderCount?: number;
+    threshold?: number;
+    root?: null | HTMLElement;
+}
+export interface Props extends CommonProps {
+    children: React.ReactNode[];
+    renderCallback?: (containerInfo: IntersectionObserverEntryFace, containerVdom?: ReactDom, index?: number) => void;
+}
+export interface ReactDom extends React.ReactElement {
+    ref?: React.RefObject<any>;
+}
+export interface IntersectionObserverEntryFace {
+    time: number;
+    rootBounds: ClientRect;
+    boundingClientRect: ClientRect;
+    intersectionRect: ClientRect;
+    intersectionRatio: number;
+    target: HTMLElement;
+    isIntersecting: boolean;
+}
+export interface IntersectionObserverCase {
+    disconnect: () => void;
+    observe: (arg0: HTMLElement) => void;
+    takeRecords: () => any[];
+    unobserve: (arg0: HTMLElement) => void;
+}
+export declare type keyI = string | React.ReactText | number;
